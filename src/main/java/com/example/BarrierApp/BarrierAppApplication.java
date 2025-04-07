225f020b8fca6d00e9944ca4218324e2faff3ad0
gen1ral <ainolhuawei@gmail.com>
2025-04-07 19:49:44 +0500

    Spring Initializr

---
+++ src/main/java/com/example/BarrierApp/BarrierAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BarrierApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrierAppApplication.class, args);
+	}
+
+}
